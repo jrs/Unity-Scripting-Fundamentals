0e9070a90228eef52d701d7352d9b96631f0fa9f
Justin Scott <hello@jrs.dev>
2023-10-01 19:21:02 -0700

    Adding Variables Script file

---
+++ Unity Scripting Project/Assets/Scripts/Variables.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Declaring Variables
+    public int Score = 0;
+    public string PlayerName = "John";
+    public float Amount = 19.95f;
+    public bool IsGameOver = false;
+    public GameObject PlayerObject;
+
+    private bool _isPlayerActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
