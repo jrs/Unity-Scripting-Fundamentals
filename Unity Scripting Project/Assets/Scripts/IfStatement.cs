1f4b1d208d4c6a69f44c673be0ec338d136a9862
Justin Scott <hello@jrs.dev>
2023-10-15 13:09:28 -0700

    Adding If Statements

---
+++ Unity Scripting Project/Assets/Scripts/IfStatement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatement : MonoBehaviour
+{
+    public int Points = 100;
+    public int RequiredPoints = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Standard if statement
+        if (Points >= RequiredPoints)
+        {
+            // run code if condition is true
+            LevelUpPlayer();
+        }
+        else
+        {
+            // run this code if the conditon was false
+            Debug.Log("You still needed points.");
+        }
+    }
+
+    private void LevelUpPlayer()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // creating the ADD condition for our if statement
+        if (collision.gameObject.CompareTag("Enemy") && collision.transform.position.y > transform.position.y)
+        {
+            // run this code if both conditions are true
+        }
+
+        // creating the OR condition for our if statement
+        if (collision.gameObject.CompareTag("Enemy") || collision.transform.position.y > transform.position.y)
+        {
+            // run this code if both conditions are true
+        }
+
+    }
+}
